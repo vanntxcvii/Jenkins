c94f6bc43f859ee91d5434dce7004b5fd456c4f0
Van Nguyen Tri <ntrivan@tma.com.vn>
2023-04-12 15:49:30 +0700

    m

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -2,7 +2,7 @@ def call() {
   def app_name = "my-python-app"
   def app_version = "1.0.${env.BUILD_NUMBER}"
   def docker_tag = "${app_name}:${app_version}"
-  def docker_registry_url = "docker.io"
+  def docker_registry_url = "elessarxcvii/my-jenkins-image"
   def docker_push_credential_id = "dockerhub_ssh_key"
 
   docker.withRegistry(docker_registry_url) {
