dad1bc38a8dff9869a0c7f51d418797abf3ca9e5
Van Nguyen Tri <ntrivan@tma.com.vn>
2023-04-12 15:39:38 +0700

    m

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -2,7 +2,7 @@ def call() {
   def app_name = "my-python-app"
   def app_version = "1.0.${env.BUILD_NUMBER}"
   def docker_tag = "${app_name}:${app_version}"
-  def docker_registry_url = "https://hub.docker.com/repository/docker/elessarxcvii/my-jenkins-image"
+  def docker_registry_url = "https://hub.docker.com/"
   def docker_push_credential_id = "dockerhublogin"
 
   docker.withRegistry(docker_registry_url, docker_push_credential_id) {
